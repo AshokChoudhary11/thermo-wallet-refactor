af787c92058a6668f97b7657051b64d9058f2992
AshokChoudhary11 <ashokchoudharya86@gmail.com>
2024-04-12 02:01:51 +0530

    Add necessary files for wallet page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/style.css';
+import Wallet from './components/Wallet'
+import Navbar from './components/Navbar'
+import FooterLinks from './components/FooterLinks'
+import BackToTop from './components/BackToTop'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     <Wallet/>
+     <FooterLinks/>
+     <BackToTop/>
     </div>
   );
 }
---
+++ src/assets/images/avatars/index.js
@@ -0,0 +1,15 @@
+import  avatar1Img from './avatar1.png'
+import avatar2Img from './avatar2.png'
+import avatar3Img from './avatar3.png'
+import avatar4Img from './avatar4.png'
+import avatar5Img from './avatar5.png'
+import avatar6Img from './avatar6.png'
+
+export {
+  avatar1Img,
+  avatar2Img,
+  avatar3Img,
+  avatar4Img,
+  avatar5Img,
+  avatar6Img
+}
